0413dc62fd09e0a8c71e7bab81e9b7ae7b94a520
Tina Ramsey <tina.ramsey87@gmail.com>
2015-02-27 12:11:33 -0800

    Add web functionality for adding reporters to beats.

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ newspaperManager.config(function($stateProvider) {
   $stateProvider.state('home', {
     url: "",
     templateUrl: "partials/home.html",
+  });
+
+  $stateProvider.state('beats', {
+    url: "/beats",
+    templateUrl: "partials/beats.html",
     controller: "BeatsCtrl"
+  });
+
+  $stateProvider.state('beats.reporters', {
+    url: "/:beatId",
+    templateUrl: "partials/beats.reporters.html",
+    controller: "ReportersCtrl"
   });
 });
